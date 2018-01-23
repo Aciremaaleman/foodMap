192ba66d0b54ca9339fb633b655724a3942b6d62
aciremaaleman <aciremaaleman@outlook.es>
2018-01-22 22:52:04 -0600

    Actualizando foodmap con imagenes cuando se filtra

--- js/app.js
+++ js/app.js
@@ -1,97 +1,42 @@
 $(document).ready(function(loadPage){
   $("#searcher").keyup(getInput);
-  
+
 });
 
   function getInput(){
     var search = $("#searcher").val().toLowerCase();
-    console.log(search);
-
-    
-    
     if($("#searcher").val().trim().length > 0){
       for(var i = 0; i > restaurants.length; i++){
         var indiceRestaurant = restaurants[i];
-        console.log(indiceRestaurant);
       };
       var filteredRestaurants = restaurants.filter(function(indiceRestaurant){
         return indiceRestaurant.food.toLowerCase().indexOf(search) >=0;
       });
+      $("#food").empty();
+        filteredRestaurants.forEach(function(indiceRestaurant) {
+            paint(indiceRestaurant);
+        });
+    } else {
+        $("#food").empty();
+        filteredRestaurants.forEach(function(indiceRestaurant) {
+            paint(indiceRestaurant);
+        });
       console.log(filteredRestaurants);
-    
     };
   };
-    
 
+  var paint = function(indiceRestaurant){
+    // var $container = $("<div />", {"class": "row"});
+    var $container = $("<div />");
+    var $title = $("<p />", {"src":indiceRestaurant.title, "class": "style-first-restaurant"});
+    var $img = $("<img />", {"src":indiceRestaurant.photo, "class": "style-img"});
+    // var $modal = $("modal" {"class":"modal fade", "id":"ventana1"})
 
-
-
-
-    //te consolea el valor del input
-  
-//     compare(search);
-//   };
-//   // Se inicializa función compare para comparar el resultado de la función anterior con la data
-//   function compare(search) {
-//     var result = restaurants.filter(function(rest){
-//       for()
-//       // if (rest.food == search) {
-//       //    return restaurants.food.toLowerCase().indexOf(search) >= 0;
-//       // }
-//       // paint(result);
-//     });
-//     console.log(result);
-//  };
-
- // function paint(result){
- //   if (result ==  ) {
- //       return contact.name.toLowerCase().indexOf(searchContact) >= 0;
- //
- //   }
- // }
-
-  // function rest(result){
-  //   if (compare == 'comida china'){
-  //     return $("#chinese_food").css('display','block');
-  //   };
-  // };
-  // console.log(rest());
-
-//cosas que ya hice y no me funcionaron las comento para que no se me olvide
-  // function compare(search){
-  //   var resultInput = restaurante.filter(function(rest){
-  //     if (rest.restaurantes == search) {
-  //       return $("#chinese_food").remove('display','none').add('display','block');
-  //
-  //     }
-  //     console.log(rest.restaurantes == search);
-  //     console.log(resultInput);
-  //   }) //utilizas el metodo filter para filtrar el input
-  //   //   return restaurant.restaurantes == search; //comparas el input con lo que hay en el objeto
-  //   // });
-  //   // console.log(resultInput);
-  // };
-
-
-
-// var restaurants = restaurante["restaurant"];
-// console.log(restaurants);
-//
-// function loadPage() {
-//   $("#searcher").keyup(filterRestaurants);
-// }
-//
-// function filterRestaurants(){
-//   var searchRestaurant = $("#searcher").val().toLowerCase();
-//   console.log(searchRestaurant);
-//   if($("#searcher").val().trim().length > 0) {
-//     var resultSearch = restaurante.filter(function(food) {
-//       return food.restaurant == searchRestaurant;
-//       console.log(resultSearch);
-//     });
-//     $("#food").css("display", "block");
-//   }
-// }
-//
-
-//inicializas el documento html con jquery
+    $title.append(indiceRestaurant.title);
+    $img.append(indiceRestaurant.photo);
+    // $container.append($containerCol);
+    $container.append($title);
+    $container.append($img);
+    // $container.append($containerCol);
+    $("#food").prepend($container);
+  }
--- js/data.js
+++ js/data.js
@@ -1,13 +1,26 @@
 var restaurants = [
-    chinese = { 
-      'food':'Comida China'},
-    mexican = {
-      'food':'Comida Mexicana'},
-    italian =  {
-      'food':'Comida Italiana'},
+    china = {
+      'food':'Comida China',
+      'photo':'../assets/images/china.jpg',
+      'title':'Comida China'
+    },
+    mexicana = {
+      'food':'Comida Mexicana',
+      'photo':'../assets/images/mexicana.jpg',
+      'title':'Comida Mexicana'
+    },
+    italiana =  {
+      'food':'Comida Italiana',
+      'photo':'../assets/images/italiana.jpg',
+      'title':'Comida Italiana'
+    },
     española =   {
-     'food':'Comida Mediterranea'},
+     'food':'Comida Mediterranea',
+     'photo':'../assets/images/mediterranea.jpg',
+     'title':'Comida Mediterranea'
+   },
     japonesa =  {
-      'food':'Comida japonesa'}]
-  
-  console.log(restaurants);
+      'food':'Comida japonesa',
+      'photo':'../assets/images/japonesa.jpg',
+      'title':'Comida japonesa'
+    }]
